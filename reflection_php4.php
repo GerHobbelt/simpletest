7f41e91c3897e175763796933e15e0ef93824abb
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2005-08-18 02:46:28 +0000

    Mock cleaning

--- reflection_php4.php
+++ reflection_php4.php
@@ -13,6 +13,13 @@
      */
     class SimpleReflection {
         
+        /**
+         *    Checks that a class or interface has been
+         *    declared.
+         *    @return boolean        True if defined.
+         *    @access public
+         *    @static
+         */
         function classOrInterfaceExists($class) {
             return class_exists($class);
         }
@@ -20,6 +27,9 @@
         /**
          *    Needed to kill the autoload feature in PHP5
          *    for classes created dynamically.
+         *    @return boolean        True if defined.
+         *    @access public
+         *    @static
          */
         function classOrInterfaceExistsSansAutoload($class) {
             if (version_compare(phpversion(), '5') >= 0) {
@@ -28,6 +38,14 @@
             return class_exists($class);
         }
         
+        /**
+         *    Gets the list of methods on a class or
+         *    interface.
+         *    @param string $class    Class or interface.
+         *    @returns array          List of method names.
+         *    @access public
+         *    @static
+         */
         function getMethods($class) {
             return get_class_methods($class);
         }
