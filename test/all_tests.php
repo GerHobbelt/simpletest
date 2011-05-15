e8d23e5acb9d22a6dab7fb17c4a52f1763293a47
Ger Hobbelt <ger@hobbelt.com>
2011-05-15 19:48:19 +0200

    give all_tests some extra time to run (the tests may otherwise timeout due to the maximum allowed runtime for PHP)

--- test/all_tests.php
+++ test/all_tests.php
@@ -3,6 +3,12 @@ require_once(dirname(__FILE__) . '/../autorun.php');
 
 class AllTests extends TestSuite {
     function __construct() {
+        /*
+        And a little hack to make sure PHP does not timeout
+        */
+        //   http://www.php.net/manual/en/info.configuration.php#ini.max-execution-time
+        set_time_limit(max(5 * 60, ini_get('max_execution_time')));
+        
         parent::__construct('All tests for SimpleTest ' . SimpleTest::getVersion());
         $this->addFile(dirname(__FILE__) . '/unit_tests.php');
         $this->addFile(dirname(__FILE__) . '/shell_test.php');
