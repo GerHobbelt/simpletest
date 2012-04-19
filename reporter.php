50fc24caabd0b87511ae61b88b37a158ac5139df
Gerard Hundman <ghundman@gmail.com>
2012-04-20 00:56:36 +0200

    undo/remove a less than smart edit; the formatting is best done by CSS alone, instead of being dependent on some (partial) HTML manipulation in the code.

--- reporter.php
+++ reporter.php
@@ -258,7 +258,7 @@ class HtmlReporter extends SimpleReporter {
 		$message = rtrim($message);
 		$message = ltrim($message, "\n\r\x0B");
         $message = htmlentities($message, ENT_COMPAT, $this->character_set);
-		$message = str_replace("\n", "\n<br />", $message);
+		//$message = str_replace("\n", "\n<br />", $message);
 		return $message;
     }
 }
